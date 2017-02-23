36134564610c5f4be63818b78e3792608b87dc02
942610 <abhijeetdon21@gmail.com>
2017-02-23 12:15:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/ak-one.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ak-one.html'));
 });
-
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
